444c49175ab9987bb012ebff97a48de1058c9399
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-18 14:21:35 +0200

    etusivut kovakoodattu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 
 import './App.css';
+import banner from "./img/donitsibanneri.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <h1>DONITSIKAUPPA</h1>
-      <button className="btn btn-danger" >Testinappi</button>
+    <div className="App container">
+      <div className="banner row">
+        <picture className="col-12">
+          <img className="img-fluid" src={banner} alt=""/>
+        </picture>
+      {/* <h1>DONITSIKAUPPA</h1>
+        <p>banneri tähän</p> */}
+      </div>
+      <div className="products row">
+        <div className="col-12">
+          <div className="row">
+            <div className="donut col-4">
+              <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+              <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+              <p>DONITSI</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="donut col-4">
+            <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+             <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+             <p>DONITSI</p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="donut col-4">
+             <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+             <p>DONITSI</p>
+            </div>
+            <div className="donut col-4">
+              <p>DONITSI</p>
+            </div>
+          </div>
+            
+          
+        </div>
+          
+      </div>
+     
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1>DONITSIKAUPPA</h1>
+            <p>Header: VALIKKO - LOGO - SEARCH - yms. kirjautuminen, ostoskori... </p>
+        </div>
+    )
+}
