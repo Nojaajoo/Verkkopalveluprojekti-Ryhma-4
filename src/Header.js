06c3275b2b024e90567036255494584e81147f18
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-21 22:37:38 +0200

    lisäsin logon, vaatii säätöä

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Header.css';
+import logo from "./img/donitsilogo.png"
 
 export default function Header() {
     return (
-        <div className="header">
-            <h1>DONITSIKAUPPA</h1>
-            <p>Header: VALIKKO - LOGO - SEARCH - yms. kirjautuminen, ostoskori... </p>
+        <div className="header container-fluid">
+            <div className="row">
+                <picture className="col-2">
+                <img className="img-fluid p-1" src={logo} alt="logo"/>
+                </picture>
+                <div className="col-10">
+                <p>Header: VALIKKO - LOGO - SEARCH - yms. kirjautuminen, ostoskori... </p>
+            </div>
+            </div>
+            
+            
+            
+            
         </div>
     )
 }
