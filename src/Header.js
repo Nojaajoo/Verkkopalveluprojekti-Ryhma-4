f17578aa3041a080ead422f4b98e71b2757d92ec
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-22 11:45:44 +0200

    logon säätöä

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,10 @@ export default function Header() {
     return (
         <div className="header container-fluid">
             <div className="row">
-                <picture className="col-2">
+                <picture className="col-5 col-sm-4 col-md-3 col-lg-1">
                 <img className="img-fluid p-1" src={logo} alt="logo"/>
                 </picture>
-                <div className="col-10">
+                <div className="col-7 col-sm-8 col-md-9 col-lg-11">
                 <p>Header: VALIKKO - LOGO - SEARCH - yms. kirjautuminen, ostoskori... </p>
             </div>
             </div>
